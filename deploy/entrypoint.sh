ef73d30716b3d8a407cb6bbcb8e1d2c01394edd2
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-19 17:17:20 +0900

    Fix entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -5,6 +5,8 @@ set -o errexit
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+rm -f tmp/pids/server.pid
+bundle exec rails s -p 3000 -b '0.0.0.0'
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
